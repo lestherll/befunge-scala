d390b4c0cb9ffa288847b63edf1e7cb79e622aad
lestherll <ljllacuna5@gmail.com>
2021-10-03 01:22:29 +0100

    added g, ? and p command

--- src/main/scala/befunge/Main.scala
+++ src/main/scala/befunge/Main.scala
@@ -3,16 +3,19 @@ package befunge
 import befunge.*
 
 import scala.io.StdIn.{readInt, readChar}
+import scala.util.Random
 import scala.collection.mutable.Stack
 import scala.util.Try
 
 @main def test: Unit = 
   Interpreter.interpret("input.befunge")
+  println()
 
 object Interpreter:
   private var stack: Stack[Int] = Stack[Int]()
   private var dir: Direction = Right
-  private var program: Array[Array[Char]] = Array[Array[Char]]()
+  private var program: Array[Array[Char]] = Array.fill(25){Array.fill(80){' '}}
+  private val directions: Array[Direction] = Array(Up, Down, Right, Left)
   var pc: (Int, Int) = (0, 0)
 
   def advance = dir match
@@ -47,13 +50,14 @@ object Interpreter:
     data
   end readFromFile
 
-  def programToString = program foreach { row => row foreach print; println }
+  def printProgram(program: Array[Array[Char]]) = program foreach { row => row foreach print; println }
 
   def interpret(filename: String) = 
     val program = readFromFile(filename)
 
-    def loop(x: Int, y: Int, _stringMode: Boolean = false): Int =
+    def loop(x: Int, y: Int, _stringMode: Boolean = false, _program: Array[Array[Char]] = program): Int =
       var stringMode = _stringMode
+      var program = _program
       val currChar = program(pc._2)(pc._1)
 
       (currChar, stringMode) match
@@ -63,7 +67,14 @@ object Interpreter:
         case ('!', false) => stack.push(if stack.pop() > 0 then 1 else 0)
         case ('_', false) => if stack.pop == 0 then updateDir('>') else updateDir('<')
         case ('|', false) => if stack.pop == 0 then updateDir('v') else updateDir('^')
-        case (':', false) => stack.push(stack.head)
+        case (':', false) => {
+          if stack.isEmpty then
+            stack.push(0)
+            stack.push(0)
+          else 
+            stack.push(stack.head)
+          end if
+        }
         case ('"', _) => stringMode = !stringMode
         case ('\\', false) => {
           val a = stack.pop
@@ -71,24 +82,39 @@ object Interpreter:
           stack.push(a)
           stack.push(b)
         }
+        case ('?', false) => directions(Random.nextInt(directions.length))
         case ('$', false) => stack.pop
         case ('#', false) => advance
-        case ('g', false) =>
-        case ('p', false) => 
+        case ('g', false) => {
+          val y = stack.pop
+          val x = stack.pop
+          if y < program.length && x < program(y).length then 
+            stack.push(program(y)(x))
+          else
+            stack.push(0)
+          end if
+        }
+        case ('p', false) => {
+          val y = stack.pop
+          val x = stack.pop
+          println(s"x=${x} y=${y}")
+          val v = stack.pop.toChar
+          program(y)(x) = v
+        }
         case ('&', false) => stack.push(readInt())
         case ('~', false) => stack.push(readChar())
-        case ('.', false) => println(stack.pop())
-        case (',', false) => println(stack.pop().toChar)
+        case ('.', false) => print(s"{stack.pop()}")
+        case (',', false) => print(s"${stack.pop().toChar}")
         case ('@', false) => return 0
         case (c, true) => stack.push(c)
         case (' ', false) | (_, false) =>
       end match
 
       advance
-      loop(pc._2, pc._1, stringMode)
+      loop(pc._2, pc._1, stringMode, program)
     end loop
     loop(pc._2, pc._1)
-//    println(stack)S
+    
   end interpret
 
   def updateDir(newDir: Char) = newDir match
